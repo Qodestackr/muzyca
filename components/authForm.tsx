63a01aece39f1b0b73265fe5caef1c4c2710a138
Wilson Gichuhi <55054375+Qodestackr@users.noreply.github.com>
2022-06-22 03:17:41 +0300

    Add files via upload

---
+++ components/authForm.tsx
@@ -0,0 +1,69 @@
+import { Box, Flex, Input, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { FC, useState } from 'react'
+import { useSWRConfig } from 'swr'
+import NextImage from 'next/image'
+import { auth } from '../lib/mutations'
+
+const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    await auth(mode, { email, password })
+    setIsLoading(false)
+    router.push('/')
+  }
+
+  return (
+    <Box height="100vh" width="100vw" bg="rgba(0,0,0,0.95)" color="white">
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+      <h1 color="#fff" fontSize="1.5rem" fontWeight="bold">Muzyca</h1>
+        {/*<NextImage src="/logo.svg" height={60} width={120} />*/}
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+           
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+            marginTop="1rem"
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+            marginTop="1rem"
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                '&:hover': {
+                  bg: 'green.300',
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default AuthForm
